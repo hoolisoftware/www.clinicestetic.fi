c77671f78fb8d7b8ecae4cee87550b119d025c72
Hooli Software <64575343+hoolisoftware@users.noreply.github.com>
2024-02-20 19:47:03 +0300

    fix: Updated API_URL

--- client/src/config/index.tsx
+++ client/src/config/index.tsx
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://localhost:8000/api/'
 // export const API_URL = '/api/'
-export const API_URL = 'https://preview.hoolisoftware.xyz/api/'
+export const API_URL = 'http://84.38.180.41/api/'
